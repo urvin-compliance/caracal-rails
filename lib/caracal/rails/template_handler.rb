30dab2addb16b17751c331723701bc6a9ee51224
Kendal Beattie <kendal@abbott-unlimited.com>
2020-01-02 09:42:51 -0600

    Stops warning message.

--- lib/caracal/rails/template_handler.rb
+++ lib/caracal/rails/template_handler.rb
@@ -1,11 +1,11 @@
 module Caracal
   module Rails
     class TemplateHandler
-      
-      def self.call(template)
+
+      def self.call(template, _source)
         "Tilt.new('#{ template.identifier }').render(self)"
       end
-      
+
     end
   end
 end
--- spec/lib/caracal/rails/template_handler_spec.rb
+++ spec/lib/caracal/rails/template_handler_spec.rb
@@ -1,21 +1,21 @@
 require 'spec_helper'
 
 describe Caracal::Rails::TemplateHandler do
-    
+
   #--------------------------------------------------------
   # Class Methods
   #--------------------------------------------------------
-  
+
   describe 'class method tests' do
-    
+
     # .call
     describe '#call' do
       let(:template) { double('template', identifier: 'show.docx.caracal') }
-      let(:actual)   { Caracal::Rails::TemplateHandler.call(template) }
-      
+      let(:actual)   { Caracal::Rails::TemplateHandler.call(template, nil) }
+
       it { expect(actual).to eq "Tilt.new('show.docx.caracal').render(self)" }
     end
-    
+
   end
-  
+
 end
